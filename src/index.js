07419461740d588dacf99455d038af0c1f678266
ABdelkarim <tahari.abdelkarim@gmail.com>
2023-12-19 15:53:59 +0100

    product page done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './css/style.css';
 import '@fortawesome/fontawesome-free/js/all.min.js';
 
 // const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item));
 
@@ -14,8 +14,28 @@ document.querySelectorAll('.add-to-card-btn').forEach(item => {
         alert('أضيف المنتج إلى عربة الشراء');
     })
 })
-// End The Most Selling
-// Start copyright date
-document.querySelector('.thisYear').textContent = `${new Date().getFullYear()}`;
-// End copyright date
+// End The Most Selling;
 
+// Start Product Page;
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.size-option').forEach(i => {
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    })
+});
+
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.color-option').forEach(i => {
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    })
+});
+// End Product Page
+
+// Start copyright date;
+// document.querySelector('.thisYear').textContent = `${new Date().getFullYear()}`;
+// End copyright date;
